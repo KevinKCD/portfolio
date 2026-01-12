593291458d56ea88312059410421a56d847fd0dc
Kevin <kdoan@qa.com>
2026-01-12 15:47:28 +0000

    Refactor: Github pages changes.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { HashRouter  } from "react-router";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
